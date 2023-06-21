1b617e76bea83a2afbbcfb02b08488afb9fab950
Saksham Singh <saksham5904@gmail.com>
2023-06-22 00:12:37 +0530

    updated README and gitignore

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # IP-connectivity-status
+
+A basic python program that when run returns the status of listed IP addresses in `IP_addresses.txt`
+- currently under development to feature a GUI consisting of tabular listing of all the said IP addresses and their status which gets periodically updated automatically, and added feature to show when a certain IP goes inactive
+
+## How to run
+- go to `/src`
+- run `main.py`
+- the status of the given IP addresses will be returned on stdout with the convention of <br>
+Hostname | IP Address | Status | Avg Latency | %Packets lost
+
--- src/main.py
+++ src/main.py
@@ -1,12 +1,4 @@
-import sys
-import os
 import subprocess
-import tkinter as tk
-from tkinter import ttk
-
-# root = tk.Tk()
-# root.title("Table Window")
-# root.geometry("500x500")
 
 ip_file = open("IP_addresses.txt", "r+")
 ip_list = ip_file.readlines()
@@ -59,28 +51,5 @@ while True:
             ip_output[index][4] = stdout[3][2][0]
             
 
-
-        # print(stdout)
         print(ip_output[index])
 
-
-#     # Creating a Table Frame
-#     table_frame = ttk.Frame(root)
-#     table_frame.pack()
-#     headers = ['Host Name', 'IP Address', 'Status', 'Avg. Response Time', 'Packet Loss']
-#     for col, header in enumerate(headers):
-#         label = ttk.Label(table_frame, text=header, relief=tk.RIDGE)
-#         label.grid(row=0, column=col, sticky=tk.NSEW)
-
-#     # Creating Table Data  
-#     for row, row_data in enumerate(ip_output):
-#         for col, cell_data in enumerate(row_data):
-#             label = ttk.Label(table_frame, text=cell_data, relief=tk.RIDGE)
-#             label.grid(row=row + 1, column=col, sticky=tk.NSEW)
-    
-#     table_frame.columnconfigure((0, 1, 2), weight=1)
-#     table_frame.rowconfigure(tuple(range(len(ip_output) + 1)), weight=1)
-
-#     root.update()
-
-# root.mainloop()
